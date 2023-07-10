e857ec5d554183c6ba3c818cb81fa7f469d1eabe
AnthonyDipopolo <anthony.dipopolo@gmail.com>
2023-07-09 20:53:41 -0400

    created server.js and addded comments

--- server.js
+++ server.js
@@ -1,42 +1,34 @@
-const express = require('express');
-const { Note, getNotes } = require('./models/notes');
+const express = require('express'); // Import the express module
 
-const app = express();
+const { Note, getNotes } = require('./models/notes'); // Import the Note class and getNotes function from the ./models/notes js
 
-app.use(express.static('./public'));
-app.use(express.urlencoded({ extended: false }));
+const app = express(); // Create a new Express application
 
+app.use(express.static('./public')); // Serve static files from the ./public directory
+app.use(express.urlencoded({ extended: false })); // Parse URL-encoded form data in requests
 
 app.get('/', (clientRequestObj, serverResponseObject) => {
-    serverResponseObject.sendFile(__dirname + '/public/index.html');
+    serverResponseObject.sendFile(__dirname + '/public/index.html'); // Send the index.html file when the root URL is accessed
 });
 
 app.get('/notes', (clientRequestObj, serverResponseObject) => {
-    // const notes = getNotes();
-
-    serverResponseObject.sendFile(__dirname + '/public/notes.html');
+    serverResponseObject.sendFile(__dirname + '/public/notes.html'); // Send the notes.html file when the /notes URL is accessed
 });
 
-
 app.post('/notes', (clientReq, serverRes) => {
-    const newNote = new Note(clientReq.body);
-    // const newTitle = new Note(clientReq.body.title);
-   
-    newNote.save();
+    const newNote = new Note(clientReq.body); // Create a new Note object with the request body data
+    newNote.save(); // Save the new note to the database through the save function
 
-    console.log(clientReq.body.note);
-    console.log(clientReq.body.title);
+    console.log(clientReq.body.note); // Log the note content to the console
+    console.log(clientReq.body.title); // Log the note title to the console
 
-    //have to respond back to the client to end the inifinite loop on the browser side 
-    serverRes.redirect('/notes'); //send them back to the homepage so the browser doesnt sit in limbo
+    serverRes.redirect('/notes'); // Redirect the client to the '/notes' URL to so the client side isn't looking for what to do next 
 });
 
 app.get('/notes/database', (clientRequestObj, serverResponseObject) => {
-    const notes = getNotes();
-
-    serverResponseObject.send(notes);
-
+    const notes = getNotes(); // Retrieve the notes from the notes.json database
+    serverResponseObject.send(notes); // Send the notes as the response
 });
 
+app.listen(3001, () => console.log('Server started on port 3001.')); // Start the server and listen on port 3001
 
-app.listen(3001, () => console.log('Server started on port 3001.'));
